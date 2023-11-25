d1556c537d90ccb6accd164e02f4ea616ad6a2a1
filip-svrcek <svrcek@cngroup.dk>
2023-11-25 11:52:42 +0100

    assets

--- src/graphics.ts
+++ src/graphics.ts
@@ -1,6 +1,11 @@
 import * as PIXI from "pixi.js";
+import { boardGridX, boardGridY } from "./main";
 
-export const drawHexagon = () => {
+export const drawHexagon = (
+  startingPoints: { x: number; y: number },
+  playerSprite: PIXI.Sprite,
+) => {
+  const { x, y } = startingPoints;
   // Create a Graphics object
   const graphics = new PIXI.Graphics();
 
@@ -14,30 +19,50 @@ export const drawHexagon = () => {
   // Calculate the points of the hexagon
   const points = [];
   for (let i = 0; i < 6; i++) {
-    const angle = (i / 6) * Math.PI * 2;
+    const angle = (i / 6) * Math.PI * 2 + Math.PI / 2;
     const x = size * Math.cos(angle);
     const y = size * Math.sin(angle);
     points.push(new PIXI.Point(x, y));
   }
 
   // Move the graphics object to the starting point
-  graphics.moveTo(points[0].x, points[0].y);
+  graphics.moveTo(points[0].x + x, points[0].y + y);
 
   // Draw the hexagon by connecting the points
   for (let i = 1; i < 6; i++) {
-    graphics.lineTo(points[i].x, points[i].y);
+    graphics.lineTo(points[i].x + x, points[i].y + y);
   }
 
   // Close the shape
-  graphics.lineTo(points[0].x, points[0].y);
+  graphics.lineTo(points[0].x + x, points[0].y + y);
 
   // End the fill and line style
   graphics.endFill();
 
+  // Make the hexagon interactive
+  graphics.interactive = true;
+  graphics.on("mouseover", () => {
+    if (graphics.tint !== 0x000000) {
+      graphics.tint = 0x00ff00;
+    }
+  });
+  graphics.on("mouseout", () => {
+    if (graphics.tint !== 0x000000) {
+      graphics.tint = 0xff0000;
+    }
+  });
+  graphics.on("click", () => {
+    playerSprite.x = graphics._bounds.minX + boardGridX;
+    playerSprite.y = graphics._bounds.minY + boardGridY;
+  });
+
   return graphics;
 };
 
-export const drawHexagonBoard = (map: number[][], playerSprite: PIXI.Sprite) => {
+export const drawHexagonBoard = (
+  map: number[][],
+  playerSprite: PIXI.Sprite,
+) => {
   const grid = new PIXI.Container();
   const size = 50;
   const points = [];
@@ -51,30 +76,8 @@ export const drawHexagonBoard = (map: number[][], playerSprite: PIXI.Sprite) => 
   let y = 0;
   for (let row = 0; row < map.length; row++) {
     for (let col = 0; col < map[row].length; col++) {
-      const graphics = new PIXI.Graphics();
-      grid.addChild(graphics);
-      graphics.beginFill(0xff0000);
-      graphics.lineStyle(2, 0x000000);
-      graphics.moveTo(points[0].x + x, points[0].y + y);
-      for (let i = 1; i < 6; i++) {
-        graphics.lineTo(points[i].x + x, points[i].y + y);
-      }
-      graphics.lineTo(points[0].x + x, points[0].y + y);
-      graphics.endFill();
-      graphics.interactive = true;
-      graphics.on("mouseover", () => {
-        if(graphics.tint !== 0x000000)
-        {graphics.tint = 0x00ff00;}
-      });
-      graphics.on("mouseout", () => {
-        if(graphics.tint !== 0x000000)
-        {graphics.tint = 0xff0000;}
-      });
-      graphics.on('click', () => {
-        playerSprite.x = graphics._bounds.minX+580;
-        playerSprite.y = graphics._bounds.minY+290;
-        console.log(graphics);
-      });
+      const hexagon = drawHexagon({ x, y }, playerSprite);
+      grid.addChild(hexagon);
       x += 1.8 * size;
     }
     if (row % 2 === 0) {
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import "./style.css";
 import * as PIXI from "pixi.js";
 
-import cartmanFront from "./assets/front.png";
+import knight from "./assets/knight/Idle (1).png";
 import { drawHexagonBoard } from "./graphics";
 
 // The application will create a renderer using WebGL, if possible,
@@ -17,47 +17,51 @@ const app = new PIXI.Application({
 document.body.appendChild(app.view as unknown as Node);
 
 // load the texture we need
-const texturePlayer = await PIXI.Assets.load(cartmanFront);
+const texturePlayer = await PIXI.Assets.load(knight);
 
 // This creates a texture
 const player = new PIXI.Sprite(texturePlayer);
 
-const boardGrid = drawHexagonBoard([
-  [1, 1, 1],
-  [1, 0, 1, 0],
-  [1, 1, 0],
-  [1, 1, 1, 0],
-], player);
+const boardGrid = drawHexagonBoard(
+  [
+    [1, 1, 1, 1, 1],
+    [1, 0, 1, 0, 1, 1],
+    [1, 1, 0, 1, 1],
+    [1, 1, 1, 0, 1, 1],
+    [1, 0, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1],
+  ],
+  player,
+);
 
 // Setup the position of the player
 player.x = app.renderer.width / 2;
 player.y = app.renderer.height / 2;
-player.scale.set(0.5, 0.5);
+player.scale.set(0.2, 0.2);
 
 const boardGridWidth = boardGrid.width;
 const boardGridHeight = boardGrid.height;
-export const boardGridX = boardGrid.x;
-export const boardGridY = boardGrid.y;
-boardGrid.x = (app.renderer.width / 2) - (boardGridWidth / 2);
-boardGrid.y = (app.renderer.height / 2) - (boardGridHeight / 2);
+export const boardGridX = app.renderer.width / 2 - boardGridWidth / 2;
+export const boardGridY = app.renderer.height / 2 - boardGridHeight / 2;
+
+boardGrid.x = boardGridX;
+boardGrid.y = boardGridY;
 
 // Populate the stage
 app.stage.addChild(boardGrid);
 app.stage.addChild(player);
 
-// Handle keyboard events
-document.addEventListener("keydown", (e) => handlePressedKeys(e));
-document.addEventListener("keyup", (e) => handlePressedKeys(e));
-let pressedKeys: string[] = [];
-function handlePressedKeys(e: KeyboardEvent) {
-  if (e.type === "keydown") {
-    !pressedKeys.includes(e.key) && pressedKeys.push(e.key);
-  } else {
-    pressedKeys = pressedKeys.filter((key) => key !== e.key);
-  }
-}
+// // Handle keyboard events
+// document.addEventListener("keydown", (e) => handlePressedKeys(e));
+// document.addEventListener("keyup", (e) => handlePressedKeys(e));
+// let pressedKeys: string[] = [];
+// function handlePressedKeys(e: KeyboardEvent) {
+//   if (e.type === "keydown") {
+//     !pressedKeys.includes(e.key) && pressedKeys.push(e.key);
+//   } else {
+//     pressedKeys = pressedKeys.filter((key) => key !== e.key);
+//   }
+// }
 
 // Listen for frame updates
-app.ticker.add(() => {
-
-});
+app.ticker.add(() => {});
